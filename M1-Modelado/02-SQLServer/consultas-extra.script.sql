b594981c82fb73d869569f783dd7de74889cc47c
Roberto Alonso <robertinage@gmail.com>
2022-11-23 19:45:14 +0100

    3QuerysLeft

--- M1-Modelado/02-SQLServer/consultas-extra.script.sql
+++ M1-Modelado/02-SQLServer/consultas-extra.script.sql
@@ -1,10 +1,16 @@
 /* Listar las pistas ordenadas por el número de veces que aparecen en playlists de forma descendente */
 /**/
-
+SELECT PT.TrackId, COUNT(*) as Reps
+FROM [LemonMusic].[dbo].PlaylistTrack PT
+	GROUP BY PT.TrackId
+	ORDER BY COUNT(*) ASC
 
 /* Listar las pistas más compradas (la tabla InvoiceLine tiene los registros de compras) */
 /**/
-
+SELECT IL.TrackId, COUNT (*) as Compras
+FROM [LemonMusic].[dbo].InvoiceLine IL
+	GROUP BY IL.TrackId
+	ORDER BY COUNT(*) DESC
 
 /* Listar los artistas más comprados */
 /**/
