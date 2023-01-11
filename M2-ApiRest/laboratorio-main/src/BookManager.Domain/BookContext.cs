14727b5c4bb79fa335371debb3f0050c1ef011e7
Roberto Alonso <robertinage@gmail.com>
2023-01-11 17:01:21 +0100

    DB Migration Done

---
+++ M2-ApiRest/laboratorio-main/src/BookManager.Domain/BookContext.cs
@@ -0,0 +1,30 @@
+﻿using BookManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Identity.Client.AppConfig;
+using System.Reflection.Metadata.Ecma335;
+
+namespace BookManager.Domain
+{
+    public class BookContext : DbContext
+    {
+        private const string ConnectionString = "Server=localhost;Database=Books;user=sa;password=Lem0nCode!;Encrypt=False";
+
+        public DbSet<BookEntity> Books { get; set; }
+        public DbSet<AuthorEntity> Authors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<BookEntity>()
+                .HasOne(a => a.Author)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
--- M2-ApiRest/laboratorio-main/src/BookManager.Domain/Entities/BookEntity.cs
+++ M2-ApiRest/laboratorio-main/src/BookManager.Domain/Entities/BookEntity.cs
@@ -3,9 +3,12 @@
     public class BookEntity
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public DateTime PublishedOn { get; set; }
-        public string? Description { get; set; }
         public int AuthorId { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public DateTime? PublishedOn { get; set; }
+        public string Description { get; set; } = String.Empty; 
+
+        // Navigation Properties
+        public AuthorEntity Author { get; set; }
     }
 }
