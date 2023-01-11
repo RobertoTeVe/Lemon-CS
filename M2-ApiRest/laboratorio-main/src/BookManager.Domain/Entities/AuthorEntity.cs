a973b0002948fb6b506e4517362a9ff683de44d9
Roberto Alonso <robertinage@gmail.com>
2023-01-11 16:11:44 +0100

    Erased Worthless Code from LaunchSettings

--- M2-ApiRest/laboratorio-main/src/BookManager.Domain/Entities/AuthorEntity.cs
+++ M2-ApiRest/laboratorio-main/src/BookManager.Domain/Entities/AuthorEntity.cs
@@ -5,13 +5,10 @@ namespace BookManager.Domain.Entities
     public class AuthorEntity
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        /// <summary>
-        /// Last name could be none, because there are books which author is unknown thus we can have an author in our db called "Anonymus"
-        /// </summary>
-        public string? LastName { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
         public DateTime? Birth { get; set; }
-        public string? CountryCode { get; set; }
+        public string CountryCode { get; set; } = String.Empty;
 
         // Navigation properties
         public List<BookEntity> Books { get; set; } = new();
