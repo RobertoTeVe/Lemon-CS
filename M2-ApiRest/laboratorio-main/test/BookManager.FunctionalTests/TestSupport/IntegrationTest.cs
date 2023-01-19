84329fbbe1fd168046ce09ebb881ee1ada3434ba
Roberto Alonso <robertinage@gmail.com>
2023-01-19 20:39:31 +0100

    Code added

---
+++ M2-ApiRest/laboratorio-main/test/BookManager.FunctionalTests/TestSupport/IntegrationTest.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using BookManager.Persistence.SqlServer;
+
+namespace BookManager.FunctionalTests.TestSupport
+{
+    internal class IntegrationTest
+        : IDisposable
+    {
+
+        private readonly IServiceProvider _serviceProvider;
+        protected HttpClient HttpClient { get; }
+        protected IntegrationTest()
+        {
+            var server =
+                new TestServer(
+                    new WebHostBuilder()
+                        .UseStartup<Startup>()
+                        .UseEnvironment("Test")
+                        //.UseCommonConfiguration()
+                        /*.ConfigureTestServices(ConfigureTestServices)*/);
+
+            HttpClient = server.CreateClient();
+
+            // Strategy to use a unique DB schema per test execution
+            _serviceProvider = server.Services;
+            // Apply Migrations
+            using var dbContext = _serviceProvider.CreateScope().ServiceProvider.GetRequiredService<BookDbContext>();
+            dbContext.Database.Migrate();
+        }
+
+
+        private void RemoveDependencyInjectionRegisteredService<TService>(IServiceCollection services)
+        {
+            var serviceDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(TService));
+            if (serviceDescriptor != null)
+            {
+                services.Remove(serviceDescriptor);
+            }
+        }
+
+        public void Dispose()
+        {   
+            using var dbContext = _serviceProvider.GetService<BookDbContext>();
+            dbContext?.Database.EnsureDeleted();
+        }
+
+    }
+}
--- M2-ApiRest/laboratorio-main/test/BookManager.FunctionalTests/UnitTest1.cs
+++ M2-ApiRest/laboratorio-main/test/BookManager.FunctionalTests/UnitTest1.cs
@@ -1,10 +1,37 @@
+using Azure;
+using BookManager.Application.Models;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text.Json.Nodes;
+
 namespace BookManager.FunctionalTests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public async Task G_New_Author_W_Created_in_DB_T_Return_The_Created_Author()
     {
+        var httpClient = new HttpClient();
 
+        Author author = new Author
+        {
+            Name = "Andreas",
+            LastName = "Spies",
+            Birth = DateTime.Now,
+            CountryCode = "DE"
+        };
+
+        var content = JsonConvert.SerializeObject(author);
+        var buffer = System.Text.Encoding.UTF8.GetBytes(content);
+        var byteContent = new ByteArrayContent(buffer);
+        byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+        var httpResponse = await httpClient.PostAsync("api/authors", byteContent);
+
+        string responseText = await httpResponse.Content.ReadAsStringAsync();
+
+        author.Should().Be(responseText);
     }
 }
