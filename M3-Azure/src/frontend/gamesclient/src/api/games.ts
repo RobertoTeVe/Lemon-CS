4fef1d3dfb8e61d35ae21b7dfc772afdc721b839
Roberto Alonso <robertinage@gmail.com>
2023-02-11 19:01:05 +0100

    Fixed typo

--- M3-Azure/src/frontend/gamesclient/src/api/games.ts
+++ M3-Azure/src/frontend/gamesclient/src/api/games.ts
@@ -2,7 +2,7 @@ import { ApiMethods } from '../constants/apiMethods';
 import { IGame } from '../models/IGame';
 import { apiFetch } from '../services/apiClient';
 
-const baseURL = 'https://lemonazureapi.azurewebsites.net//api/';
+const baseURL = 'https://lemonazureapi.azurewebsites.net/api/';
 
 export class GamesApi {
     async get(): Promise<IGame[]> {
